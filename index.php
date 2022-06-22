06a6041ce80dd30eee177aef2d250e580afa048f
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-22 13:37:06 +0200

    index.php file added

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- - creare una pagina index.php dove visualizzare nomi di tutti i dipartimenti;
+- al click sul link dei dettagli di ogni dipartimento visualizzare una pagina con le informazioni dettagliate del dipartimento cliccato.
+Usare mysqli per prelevare i dati dal database;
+Creare una classe del dipartimento e usarla per stampare i dati nelle pagine. -->
+<?php
+// require_once __DIR__ . '/Department.php';
+// require_once __DIR__ . '/database.php';
+
+echo 'ciao a tutti';
+?>
